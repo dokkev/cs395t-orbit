1462b44336ba15db79fc5438c987765c763e5088
Walter Stark <walterzstark@gmail.com>
2024-03-08 01:10:01 -0600

    Added new lines to the init code for "Isaac-Manipulate-Franka" to allow us to do RL training.  Added observation and reward classes  to the subpackages with the previously mentioned gym name. Added an mdp folder as well

--- source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/mycode/manipulate/config/franka/__init__.py
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/mycode/manipulate/config/franka/__init__.py
@@ -20,6 +20,8 @@ gym.register(
         "env_cfg_entry_point": joint_pos_env_cfg.FrankaCubeLiftEnvCfg,
         "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.LiftCubePPORunnerCfg,
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
     },
     disable_env_checker=True,
 )
